699f49409b4254bea906e95ea7b75dec27376a25
Sn0rkY <joffrey.czarny@eads.net>
2017-04-21 19:32:04 +0200

    Add ps scripts to call RFC function module via SOAP.

---
+++ Standalone/soap/rfc/Invoke-soap_rfc_ping.ps1
@@ -0,0 +1,74 @@
+  <#
+    .Synopsis
+      Calls SOAP RFC_PING function 
+    .Description
+      Calls the function RFC_PING, through the /sap/bc/soap/rfc SOAP service, 
+      to test connectivity to remote RFC destinations.
+    .Author
+      @_Sn0rkY
+
+    All credit to Agnivesh Sathasivam & @nmonkee  
+#>
+
+    #-Set Parameters
+    Param(
+    [parameter(Mandatory = $true)]
+    [alias("u")]
+    [string]$username,
+
+    [parameter(Mandatory = $true)]
+    [alias("t")]
+    [string]$target,
+
+    [parameter(Mandatory = $true)]
+    [alias("c")]
+    [string]$client,
+  
+    [parameter(Mandatory = $true)]
+    [string]$password = $( Read-Host -asSecureString "Input password" )
+    )
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+function Execute-SOAPRequest 
+{ 
+$user = $username
+$pass = $password 
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+        'SOAPAction' = 'urn:sap-com:document:sap:rfc:functions'
+        'sap-client' = $client 
+        'sap-language' = 'EN'
+        Authorization = $basicAuthValue
+    }
+
+$postParams = '<?xml version="1.0" encoding="utf-8" ?><env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><env:Body><n1:RFC_PING xmlns:n1="urn:sap-com:document:sap:rfc:functions" env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"></n1:RFC_PING></env:Body></env:Envelope>'
+
+$soapreq = Invoke-WebRequest $url -Method $method -ContentType "text/xml" -Body $postParams -Headers $Headers
+
+WriteXmlToScreen $soapreq.Content
+} 
+
+#-Main
+
+$url = 'http://'+$target+':8000/sap/bc/soap/rfc'
+$method = 'post'
+
+Execute-SOAPRequest  
+
+#-End
+
---
+++ Standalone/soap/rfc/Invoke-soap_rfc_read_table.ps1
@@ -0,0 +1,79 @@
+<#
+    .Synopsis
+      Calls SOAP RFC_READ_TABLE function 
+    .Description
+      Calls the function RFC_READ_TABLE, through the /sap/bc/soap/rfc SOAP 
+      service, to read data from tables.
+      /!\ Hardcoded USR02 table /!\
+    .Author
+      @_Sn0rkY
+
+    All credit to Agnivesh Sathasivam & @nmonkee  
+#>
+
+#-Set Parameters
+    Param(
+    [parameter(Mandatory = $true)]
+    [alias("u")]
+    [string]$username,
+
+    [parameter(Mandatory = $true)]
+    [alias("t")]
+    [string]$target,
+
+    [parameter(Mandatory = $true)]
+    [alias("c")]
+    [string]$client,
+  
+    [parameter(Mandatory = $true)]
+    [string]$password = $( Read-Host -asSecureString "Input password" )
+    )
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+function Execute-SOAPRequest 
+{ 
+$user = $username
+$pass = $password 
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+        'SOAPAction' = 'urn:sap-com:document:sap:rfc:functions'
+        'sap-client' = $client 
+        'sap-language' = 'EN'
+        Authorization = $basicAuthValue
+    }
+
+$postParams = '<?xml version="1.0" encoding="utf-8" ?><env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><env:Body><n1:RFC_READ_TABLE xmlns:n1="urn:sap-com:document:sap:rfc:functions" env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><DELIMITER xsi:type="xsd:string">|</DELIMITER><NO_DATA xsi:nil="true"></NO_DATA><QUERY_TABLE xsi:type="xsd:string">USR02</QUERY_TABLE><DATA xsi:nil="true"></DATA><FIELDS xsi:nil="true"><item><FIELDNAME>BNAME</FIELDNAME></item><item><FIELDNAME>BCODE</FIELDNAME></item></FIELDS><OPTIONS xsi:nil="true"></OPTIONS></n1:RFC_READ_TABLE></env:Body></env:Envelope>'
+
+#$soapreq = Invoke-WebRequest $url -Method $method -ContentType "text/xml" -InFile RFC_READ_TABLE2.xml -Headers $Headers
+$soapreq = Invoke-WebRequest $url -Method $method -ContentType "text/xml" -Body $postParams -Headers $Headers
+
+
+
+WriteXmlToScreen $soapreq.Content
+#Write-Host $soapreq.Content
+} 
+
+#-Main
+
+$url = 'http://172.16.141.12:8000/sap/bc/soap/rfc'
+$method = 'post'
+
+Execute-SOAPRequest  
+
+#-End
+
