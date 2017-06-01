51dbcdf80f447308e7c43283c426dd5c85f86473
Sn0rkY <joffrey.czarny@eads.net>
2017-06-01 20:05:53 +0200

    add comment on systemnumber

--- Standalone/rfc/Invoke-RFC_bruteforce.ps1
+++ Standalone/rfc/Invoke-RFC_bruteforce.ps1
@@ -36,6 +36,7 @@
         $cfgParams = New-Object SAP.Middleware.Connector.RfcConfigParameters
         $cfgParams.Add("NAME", "TEST")
         $cfgParams.Add("ASHOST", $target)
+       # systemnumber is by default 00 
         $cfgParams.Add("SYSNR", "00")
         $cfgParams.Add("CLIENT", $client)
         $cfgParams.Add("USER", $username)
