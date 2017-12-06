c0076de096701da8359b7800f5ce2d0da624898d
Sn0rkY <snorky@insomnihack.net>
2017-12-06 02:19:17 +0100

    Retrieve SAP user info via BAPI_USER_GET_DETAIL by @_1ggy

---
+++ Standalone/soap/rfc/Invoke-soap_rfc_USER_DETAIL.ps1
@@ -0,0 +1,82 @@
+<#
+    .Synopsis
+      Calls SOAP BAPI_USER_GET_DETAIL
+    .Description
+      Retrieve SAP user information
+      Calls the function BAPI_USER_GET_DETAIL, through the /sap/bc/soap/rfc SOAP 
+      $target like : http(s)://ip:port
+    .Author
+      @_1ggy
+#>
+
+#-Set Parameters
+    Param(
+    [parameter(Mandatory = $true, HelpMessage="target like : http(s)://ip:port")]
+    [alias("t")]
+    [string]$target,
+
+    [parameter(Mandatory = $true)]
+    [alias("c")]
+    [string]$client,
+  
+    [parameter(Mandatory = $true)]
+    [alias("u")]
+    [string]$username,
+    
+    [parameter(Mandatory = $true)]
+    [string]$password = $( Read-Host -asSecureString "Input password" ),
+    
+    [parameter(Mandatory = $true)]
+    [alias("s")]
+    [string]$sap_user
+    )
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+function Execute-SOAPRequest 
+{ 
+
+$pair = "$($username):$($password)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+        'SOAPAction' = 'urn:sap-com:document:sap:rfc:functions'
+        'sap-client' = $client 
+        'sap-language' = 'EN'
+        Authorization = $basicAuthValue
+    }
+
+$postParams = '<?xml version="1.0" encoding="utf-8" ?>
+<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<env:Body>
+    <n1:BAPI_USER_GET_DETAIL xmlns:n1="urn:sap-com:document:sap:rfc:functions" env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <USERNAME>'+$sap_user+'</USERNAME>
+    </n1:BAPI_USER_GET_DETAIL>
+</env:Body>
+</env:Envelope>'
+
+$soapreq = Invoke-WebRequest $url -Method $method -ContentType "text/xml" -Body $postParams -Headers $Headers
+
+WriteXmlToScreen $soapreq.Content
+} 
+
+#-Main
+
+$url = $target+'/sap/bc/soap/rfc'
+$method = 'post'
+
+Execute-SOAPRequest  
+
+#-End
+
