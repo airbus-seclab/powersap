782b48f84f19cdfc2ceb6920aa826ea5b468c2cd
Sn0rkY <snorky@insomnihack.net>
2017-12-06 01:06:24 +0100

    List current users connected via TH_USER_LIST FM by @_1ggy

---
+++ Standalone/soap/rfc/Invoke-soap_rfc_CONNECTED_USERS.ps1
@@ -0,0 +1,88 @@
+<#
+    .Synopsis
+      Calls SOAP TH_USER_LIST
+    .Description
+      List current users connected on SAP target
+      Gathering information about hostname, guiversion, etc
+      Calls the function TH_USER_LIST, through the /sap/bc/soap/rfc SOAP 
+      $target like : http(s)://ip:port
+    .Author
+      @_1ggy
+#>
+
+#-Set Parameters
+    Param(
+    [parameter(Mandatory = $true, HelpMessage="target like : http(s)://ip:port")]
+    [alias("t")]
+    [string]$target,
+
+    [parameter(Mandatory = $true)]
+    [alias("c")]
+    [string]$client,
+  
+    [parameter(Mandatory = $true)]
+    [alias("u")]
+    [string]$username,
+    
+    [parameter(Mandatory = $true)]
+    [string]$password = $( Read-Host -asSecureString "Input password" )
+    )
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+function Execute-SOAPRequest 
+{ 
+
+$pair = "$($username):$($password)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+        'SOAPAction' = 'urn:sap-com:document:sap:rfc:functions'
+        'sap-client' = $client 
+        'sap-language' = 'EN'
+        Authorization = $basicAuthValue
+    }
+
+$postParams = '<?xml version="1.0" encoding="utf-8" ?>
+<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<env:Body>
+    <n1:TH_USER_LIST xmlns:n1="urn:sap-com:document:sap:rfc:functions" env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <USRLIST>
+            <item>
+                <MANDT></MANDT>
+                <BNAME></BNAME>
+                <TCODE></TCODE>
+                <TERM></TERM>
+                <HOSTADDR></HOSTADDR>
+                <GUIVERSION></GUIVERSION>
+            </item>
+        </USRLIST>
+    </n1:TH_USER_LIST>
+</env:Body>
+</env:Envelope>'
+
+$soapreq = Invoke-WebRequest $url -Method $method -ContentType "text/xml" -Body $postParams -Headers $Headers
+
+WriteXmlToScreen $soapreq.Content
+} 
+
+#-Main
+
+$url = $target+'/sap/bc/soap/rfc'
+$method = 'post'
+
+Execute-SOAPRequest  
+
+#-End
+
