cc57fb404bc7f09806e8601a66e72a5832d8e6d4
Sn0rkY <snorky@insomnihack.net>
2017-12-06 02:16:21 +0100

    Retrieve SAP system info via RFC_SYSTEM_INFO FM by @_1ggy

---
+++ Standalone/soap/rfc/Invoke-soap_rfc_SYSTEM_INFO.ps1
@@ -0,0 +1,81 @@
+<#
+    .Synopsis
+      Calls SOAP RFC_SYSTEM_INFO
+    .Description
+      Retrieve SAP System information
+      Calls the function RFC_SYSTEM_INFO, through the /sap/bc/soap/rfc SOAP 
+      $target like : http(s)://ip:port
+    .Author
+      @_1ggy
+#>
+
+#-Set Parameters
+    Param(
+    [parameter(Mandatory = $true, HelpMessage="target like : http(s)://ip:port")]
+    [alias("t")]
+    [string]$target,
+
+    [parameter(Mandatory = $true)]
+    [alias("c")]
+    [string]$client,
+  
+    [parameter(Mandatory = $true)]
+    [alias("u")]
+    [string]$username,
+    
+    [parameter(Mandatory = $true)]
+    [string]$password = $( Read-Host -asSecureString "Input password" )
+    )
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+function Execute-SOAPRequest 
+{ 
+
+$pair = "$($username):$($password)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+        'SOAPAction' = 'urn:sap-com:document:sap:rfc:functions'
+        'sap-client' = $client 
+        'sap-language' = 'EN'
+        Authorization = $basicAuthValue
+    }
+
+$postParams = '<?xml version="1.0" encoding="utf-8" ?>
+<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<env:Body>
+    <n1:RFC_SYSTEM_INFO xmlns:n1="urn:sap-com:document:sap:rfc:functions" env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <CURRENT_RESOURCES xsi:nil="true"></CURRENT_RESOURCES>
+        <MAXIMAL_RESOURCES xsi:nil="true"></MAXIMAL_RESOURCES>
+        <RECOMMENDED_DELAY xsi:nil="true"></RECOMMENDED_DELAY>
+        <RFCSI_EXPORT xsi:nil="true"></RFCSI_EXPORT>
+    </n1:RFC_SYSTEM_INFO>
+</env:Body>
+</env:Envelope>'
+
+$soapreq = Invoke-WebRequest $url -Method $method -ContentType "text/xml" -Body $postParams -Headers $Headers
+
+WriteXmlToScreen $soapreq.Content
+} 
+
+#-Main
+
+$url = $target+'/sap/bc/soap/rfc'
+$method = 'post'
+
+Execute-SOAPRequest  
+
+#-End
+
